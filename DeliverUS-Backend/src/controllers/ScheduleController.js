0507056459a4b0b4b0ff9a9267239b4e65cfd2dc
rlazmar05 <rublazmar@alum.us.es>
2026-03-24 20:52:30 +0100

    controller done

--- DeliverUS-Backend/src/controllers/ScheduleController.js
+++ DeliverUS-Backend/src/controllers/ScheduleController.js
@@ -1,19 +1,53 @@
 import { Schedule } from '../models/models.js'
 
 const indexRestaurant = async function (req, res) {
-  res.status(500).send('To be implemented')
+  try {
+    const schedules = await Schedule.findAll({
+
+      where: {
+        restaurantId : req.params.restaurantId
+      }
+    })
+    res.json(schedules)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const create = async function (req, res) {
-  res.status(500).send('To be implemented')
+  const newSchedule = Schedule.build(req.body)
+  newSchedule.restaurantId = req.params.restaurantId
+  try {
+    const schedules = await newSchedule.save()
+    res.json(schedules)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const update = async function (req, res) {
-  res.status(500).send('To be implemented')
+  try {
+    await Schedule.update(req.body, { where: { id: req.params.scheduleId } })
+    const updatedSchedule = await Schedule.findByPk(req.params.scheduleId)
+    res.json(updatedSchedule)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const destroy = async function (req, res) {
-  res.status(500).send('To be implemented')
+  try {
+    const result = await Schedule.destroy({ where: { id: req.params.scheduleId } })
+    let message = ''
+    if (result === 1) {
+      message = 'Sucessfuly deleted schedule id.' + req.params.scheduleId
+    } else {
+      message = 'Could not delete schedule.'
+    }
+    res.json(message)
+  } catch (err) {
+    res.status(500).send(err)
+  }
 }
 
 const ScheduleController = {
