3917787e17c49851ffcc2f228118d9378bdd3916
rlazmar05 <rublazmar@alum.us.es>
2026-03-24 21:50:36 +0100

    Validaciones de productos

--- DeliverUS-Backend/src/controllers/validation/ProductValidation.js
+++ DeliverUS-Backend/src/controllers/validation/ProductValidation.js
@@ -16,11 +16,30 @@ const checkRestaurantExists = async (value, { req }) => {
 }
 
 const checkScheduleBelongsToRestaurantOnCreate = async (value, { req }) => {
-
+  if (!value) return Promise.resolve()
+  
+  const schedule = await Schedule.findByPk(value)
+  if (!schedule) {
+    return Promise.reject(new Error('The schedule Id does not exist'))
+  }
+  if (schedule.restaurantId !== req.body.restaurantId) {
+    return Promise.reject(new Error('The schedule does not belong to the restaurantId gave'))
+  }
+  return Promise.resolve()
 }
 
 const checkScheduleBelongsToRestaurantOnUpdate = async (value, { req }) => {
-
+  if (!value) return Promise.resolve()
+  
+  const schedule = await Schedule.findByPk(value)
+  if (!schedule) {
+    return Promise.reject(new Error('The schedule Id does not exist'))
+  }
+  const product = await Product.findByPk(req.params.productId)
+  if (product.restaurantId !== schedule.restaurantId) {
+    return Promise.reject(new Error('The schedule does not belong to the restaurant of this product'))
+  }
+  return Promise.resolve()
 }
 
 const create = [
